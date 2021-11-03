8114d4015f7138fe4b4d06d48e262c86dafebf66
Cory House <chouse@athenahealth.com>
2021-11-03 13:33:14 -0500

    Add features

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,25 @@ import Root from "@athena/forge/Root";
 import "@athena/forge/dist/forge.css";
 import Textarea from "@athena/forge/Textarea";
 import FormField from "@athena/forge/FormField";
+import Button from "@athena/forge/Button";
+import { useState } from "react";
 
 export function App() {
+  const [message, setMessage] = useState("");
+
   return (
     <Root>
       <h1>Chat</h1>
-      <Form buttonText="Send">
-        <FormField labelText="Message" id="message" inputAs={Textarea} />
+      <Form includeSubmitButton={false}>
+        <FormField
+          labelAlwaysAbove
+          labelText="Message"
+          id="message"
+          inputAs={Textarea}
+          value={message}
+          onChange={(event) => setMessage(event.currentTarget.value)}
+        />
+        <Button type="submit" text="Send" disabled={!message} />
       </Form>
     </Root>
   );
