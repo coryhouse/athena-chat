7479f14e2553ef5c706adf9967538d27beb9a8b7
Cory House <chouse@athenahealth.com>
2021-11-03 13:33:06 -0500

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { App } from "./App";
 
 describe("App", () => {
@@ -10,5 +11,19 @@ describe("App", () => {
   it("should render a Submit button with a label of 'Send'", () => {
     render(<App />);
     screen.getByRole("button", { name: "Send" });
+  });
+
+  it("should set aria-disabled on the Send button when the Message textarea is empty", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    expect(sendButton).toHaveAttribute("aria-disabled", "true");
+  });
+
+  it("should NOT disable the Send button when the Message textarea is populated", () => {
+    render(<App />);
+    const sendButton = screen.getByRole("button", { name: "Send" });
+    const messageInput = screen.getByLabelText("Message");
+    userEvent.type(messageInput, "example message");
+    expect(sendButton).toHaveAttribute("aria-disabled", "false");
   });
 });
