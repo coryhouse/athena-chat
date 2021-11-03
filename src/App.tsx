9c4e74bebeda7811a22ed31ae9520b61085ed5b4
Cory House <chouse@athenahealth.com>
2021-11-03 11:27:34 -0500

    Add tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+// Named import
 import Form from "@athena/forge/Form";
 import Root from "@athena/forge/Root";
 import "@athena/forge/dist/forge.css";
+import Textarea from "@athena/forge/Textarea";
+import FormField from "@athena/forge/FormField";
 
 export function App() {
   return (
     <Root>
       <h1>Chat</h1>
-      <Form></Form>
+      <Form>
+        <FormField labelText="Message" id="message" inputAs={Textarea} />
+      </Form>
     </Root>
   );
 }
