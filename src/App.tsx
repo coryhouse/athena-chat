2f7c996b1b36f6b5026bc1f3f15c78adfe06225c
Cory House <chouse@athenahealth.com>
2021-11-03 10:56:52 -0500

    Styled Forge components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import Form from "@athena/forge/Form";
+import Root from "@athena/forge/Root";
+import "@athena/forge/dist/forge.css";
+
 export function App() {
-  return <h1>Chat</h1>;
+  return (
+    <Root>
+      <h1>Chat</h1>
+      <Form></Form>
+    </Root>
+  );
 }
