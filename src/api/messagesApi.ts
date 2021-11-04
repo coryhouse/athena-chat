13de12c96fb67747d99da3e8b0230e26bbd046b5
Cory House <chouse@athenahealth.com>
2021-11-04 09:25:05 -0500

    sendMessage

--- src/api/messagesApi.ts
+++ src/api/messagesApi.ts
@@ -1,7 +1,21 @@
-import { SentMessage } from "../types";
+import { SentMessage, UnsentMessage } from "../types";
 
 export async function getMessages(): Promise<SentMessage[]> {
   const resp = await fetch("http://localhost:3001/messages");
+  if (!resp.ok) throw resp;
+  return resp.json();
+}
+
+export async function sendMessage(
+  unsentMessage: UnsentMessage
+): Promise<SentMessage> {
+  const resp = await fetch("http://localhost:3001/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(unsentMessage),
+  });
   if (!resp.ok) throw resp;
   return resp.json();
 }
