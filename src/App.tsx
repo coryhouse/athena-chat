6716df4e9dc3afc82bc247f67c8231a5f7db2412
Cory House <chouse@athenahealth.com>
2021-11-04 10:38:04 -0500

    Extract component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 // Named import
-import Form from "@athena/forge/Form";
 import Root from "@athena/forge/Root";
 import "@athena/forge/dist/forge.css";
-import Textarea from "@athena/forge/Textarea";
-import FormField from "@athena/forge/FormField";
-import Button from "@athena/forge/Button";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import List from "@athena/forge/List";
 import ListItem from "@athena/forge/ListItem";
 import { getMessages, sendMessage } from "./api/messagesApi";
 import { SentMessage, UnsentMessage } from "./types";
+import { MessageForm } from "./MessageForm";
 
 export function App() {
-  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<SentMessage[]>([]);
 
   useEffect(() => {
@@ -22,6 +18,12 @@ export function App() {
     }
     getInitialMessages();
   }, []); // Only run this once.
+
+  async function handleSubmit(unsentMessage: UnsentMessage): Promise<void> {
+    // TODO: Handle loading state. Consider optimistic update.
+    const sentMessage = await sendMessage(unsentMessage);
+    setMessages([...messages, sentMessage]);
+  }
 
   return (
     <Root>
@@ -33,36 +35,7 @@ export function App() {
         ))}
       </List>
 
-      <Form
-        includeSubmitButton={false}
-        onSubmit={async (event) => {
-          event.preventDefault(); // Don't post back to the server
-
-          const unsentMessage: UnsentMessage = {
-            date: new Date().toISOString(),
-            message,
-            // HACK: Hard coding ids. TODO: Fix.
-            recipientUserId: 1,
-            senderUserId: 2,
-          };
-
-          // TODO: Handle loading state. Consider optimistic update.
-          const sentMessage = await sendMessage(unsentMessage);
-
-          setMessages([...messages, sentMessage]);
-          setMessage(""); // clear the message input since it was just submitted
-        }}
-      >
-        <FormField
-          labelAlwaysAbove
-          labelText="Message"
-          id="message"
-          inputAs={Textarea}
-          value={message}
-          onChange={(event) => setMessage(event.currentTarget.value)}
-        />
-        <Button type="submit" text="Send" disabled={!message} />
-      </Form>
+      <MessageForm onSubmit={handleSubmit} />
     </Root>
   );
 }
