b0556b20b0f1bc13832687f0f0b07049b700fc81
Cory House <chouse@athenahealth.com>
2021-11-04 13:30:09 -0500

    Enhance tests

--- cypress/integration/App.spec.tsx
+++ cypress/integration/App.spec.tsx
@@ -3,5 +3,19 @@
 describe("App", () => {
   it("should display messages for the default user", () => {
     cy.visit("http://localhost:3000");
+    cy.findByText("Hi jeffsalinas");
+  });
+
+  it("should support changing the user via DevTools", () => {
+    cy.visit("http://localhost:3000");
+
+    // First, the default user should display
+    cy.findByText("Hi jeffsalinas");
+
+    // Now, let's change the user
+    cy.findByLabelText("Select user").select("coryhouse");
+
+    // Now only Cory's messages should display
+    cy.findByText("Hi Cory, it's Jeff");
   });
 });
