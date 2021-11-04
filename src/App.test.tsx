c1475bab9e83603e6b4cd599822f5727e1f2f129
Cory House <chouse@athenahealth.com>
2021-11-04 10:43:04 -0500

    Move tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,26 +21,6 @@ describe("App", () => {
     (fetch as jest.Mock).mockClear();
   });
 
-  it("should display a message input with an accessible label", () => {
-    screen.getByLabelText("Message");
-  });
-
-  it("should render a Submit button with a label of 'Send'", () => {
-    screen.getByRole("button", { name: "Send" });
-  });
-
-  it("should set aria-disabled on the Send button when the Message textarea is empty", () => {
-    const sendButton = screen.getByRole("button", { name: "Send" });
-    expect(sendButton).toHaveAttribute("aria-disabled", "true");
-  });
-
-  it("should NOT disable the Send button when the Message textarea is populated", () => {
-    const sendButton = screen.getByRole("button", { name: "Send" });
-    const messageInput = screen.getByLabelText("Message");
-    userEvent.type(messageInput, "example message");
-    expect(sendButton).toHaveAttribute("aria-disabled", "false");
-  });
-
   it.skip("should clear out the message textarea when 'Send' is clicked", async () => {
     const sendButton = screen.getByRole("button", { name: "Send" });
     const messageInput = screen.getByLabelText("Message");
