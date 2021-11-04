913887186dc51ee9e462fa46b1461914cce956a7
Cory House <chouse@athenahealth.com>
2021-11-04 11:25:47 -0500

    Add props

--- src/DevTools.tsx
+++ src/DevTools.tsx
@@ -4,12 +4,15 @@ import Select from "@athena/forge/Select";
 import { User } from "./types";
 
 type DevToolsProps = {
+  user: User;
   users: User[];
+  setUser: (user: User) => void;
 };
 
-export function DevTools({ users }: DevToolsProps) {
+export function DevTools({ users, setUser, user }: DevToolsProps) {
   return (
     <Form
+      includeSubmitButton={false}
       style={{
         position: "absolute",
         bottom: 0,
@@ -21,7 +24,14 @@ export function DevTools({ users }: DevToolsProps) {
         inputAs={Select}
         id="select-user"
         labelText="Select user"
+        onChange={(event) => {
+          const user = users.find(
+            (u) => u.username === event.currentTarget.value
+          );
+          setUser(user!);
+        }}
         options={users.map((u) => u.username)}
+        value={user.username}
       />
     </Form>
   );
