33f71fb341fa0ff24545b7351475aa7fe5683a85
Cory House <chouse@athenahealth.com>
2021-11-05 10:25:09 -0500

    Lifted state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import { Route, Routes, Link } from "react-router-dom";
 import { About } from "./About";
 import { Chat } from "./Chat";
+import Root from "@athena/forge/Root";
+import React, { Suspense, useEffect, useState } from "react";
+import { SentMessage, User } from "./types";
+import { getUsers } from "./api/userApi";
+import { getMessages } from "./api/messagesApi";
+
+// Lazy loading dev tools so that production users never get this code.
+const DevTools = React.lazy(() => import("./DevTools"));
 
 export function App() {
+  const [messages, setMessages] = useState<SentMessage[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>({
+    id: 3,
+    username: "jeffsalinas",
+  });
+
+  useEffect(() => {
+    async function getInitialData() {
+      const _messages = await getMessages(user.id);
+      setMessages(_messages);
+    }
+    getInitialData();
+  }, [user.id]); // Re-run when the user.id changes.
+
+  // TODO: This belongs lower.
+  useEffect(() => {
+    async function init() {
+      const _users = await getUsers();
+      setUsers(_users);
+    }
+    init();
+  }, []); // only run once
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Chat</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <Root>
+      <header style={{ backgroundColor: "orange", padding: 16 }}>
+        <h2>Hi {user.username}</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Chat</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
-        <Route path="/" element={<Chat />} />
+        <Route
+          path="/"
+          element={<Chat messages={messages} setMessages={setMessages} />}
+        />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+
+      <Suspense fallback="Loading...">
+        {process.env.REACT_APP_SHOW_DEV_TOOLS === "Y" && (
+          <DevTools
+            users={users}
+            setUser={setUser}
+            user={user}
+            messages={messages}
+            setMessages={setMessages}
+          />
+        )}
+      </Suspense>
+    </Root>
   );
 }
--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,40 +1,16 @@
-import Root from "@athena/forge/Root";
 import "@athena/forge/dist/forge.css";
-import React, { Suspense, useEffect, useState } from "react";
 import List from "@athena/forge/List";
 import ListItem from "@athena/forge/ListItem";
-import { getMessages, sendMessage } from "./api/messagesApi";
-import { SentMessage, UnsentMessage, User } from "./types";
+import { sendMessage } from "./api/messagesApi";
+import { SentMessage, UnsentMessage } from "./types";
 import { MessageForm } from "./MessageForm";
-import { getUsers } from "./api/userApi";
 
-// Lazy loading dev tools so that production users never get this code.
-const DevTools = React.lazy(() => import("./DevTools"));
+type ChatProps = {
+  messages: SentMessage[];
+  setMessages: (messages: SentMessage[]) => void;
+};
 
-export function Chat() {
-  const [messages, setMessages] = useState<SentMessage[]>([]);
-  const [users, setUsers] = useState<User[]>([]);
-  const [user, setUser] = useState<User>({
-    id: 3,
-    username: "jeffsalinas",
-  });
-
-  useEffect(() => {
-    async function init() {
-      const _users = await getUsers();
-      setUsers(_users);
-    }
-    init();
-  }, []); // only run once
-
-  useEffect(() => {
-    async function getInitialData() {
-      const _messages = await getMessages(user.id);
-      setMessages(_messages);
-    }
-    getInitialData();
-  }, [user.id]); // Re-run when the user.id changes.
-
+export function Chat({ messages, setMessages }: ChatProps) {
   async function handleSubmit(unsentMessage: UnsentMessage): Promise<void> {
     // TODO: Handle loading state. Consider optimistic update.
     const sentMessage = await sendMessage(unsentMessage);
@@ -42,11 +18,7 @@ export function Chat() {
   }
 
   return (
-    <Root>
-      <header style={{ backgroundColor: "orange", padding: 16 }}>
-        <h2>Hi {user.username}</h2>
-      </header>
-
+    <>
       <section aria-labelledby="chat-heading" id="chat-section" role="article">
         <h1 id="chat-heading">Chat</h1>
 
@@ -58,18 +30,6 @@ export function Chat() {
 
         <MessageForm onSubmit={handleSubmit} />
       </section>
-
-      <Suspense fallback="Loading...">
-        {process.env.REACT_APP_SHOW_DEV_TOOLS === "Y" && (
-          <DevTools
-            users={users}
-            setUser={setUser}
-            user={user}
-            messages={messages}
-            setMessages={setMessages}
-          />
-        )}
-      </Suspense>
-    </Root>
+    </>
   );
 }
