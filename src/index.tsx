037636e177efa46eaa68622606cfe5603aca2e47
Cory House <chouse@athenahealth.com>
2021-11-05 11:04:49 -0500

    Add error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { render } from "react-dom";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ErrorBoundary fallback={<>Oops, sorry an error occurred.</>}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>,
   document.getElementById("root")
 );
